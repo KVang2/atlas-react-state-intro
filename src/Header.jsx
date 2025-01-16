35653c68444a9846107c2c97634de61f769ef938
kvang2 <kevinvaj88@hotmail.com>
2025-01-15 19:08:01 -0600

    Added course count in the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
 import logo from "./assets/logo.png";
+import { useappContext } from './appContext';
 
 export default function Header() {
+  const { enrolledCourses } = useappContext();
+
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
-      <div className="enrollment">Classes Enrolled: 1</div>
+      <div className="enrollment">Classes Enrolled: {enrolledCourses.length}</div>
     </div>
   );
 }
